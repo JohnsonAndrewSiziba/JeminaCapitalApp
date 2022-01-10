9f864ab2044848b7faed516a11301111eb0c1330
Ender <siziba.uz@gmail.com>
2022-01-10 11:10:54 +0200

    Create Login Page Component

--- src/components/Auth/Input.vue
+++ src/components/Auth/Input.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <ion-item class="ion-margin-bottom">
+    <ion-input type="text" placeholder="Awesome Input"></ion-input>
+  </ion-item>
 </template>
 
 <script>
@@ -9,5 +11,23 @@ export default {
 </script>
 
 <style scoped>
+  ion-item {
+    border-radius: 50px;
+    height: 55px;
+    box-shadow: 1px 8px 8px 0px rgba(0, 0, 0, 0.08);
+    --highlight-height: 0px;
+  }
 
+  ion-input {
+    font-size: 18px;
+    --padding-top: 20px;
+    --padding-bottom: 15px;
+    --padding-start: 5px;
+  }
+
+  a {
+    font-size: 17px;
+    letter-spacing: 0.5px;
+    text-decoration: none;
+  }
 </style>
